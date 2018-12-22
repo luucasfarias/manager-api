ddbdf54ea3fc216deb96270b3b036e6ff3acaa4a
lucas farias <luucasfarias21@gmail.com>
2018-12-21 22:21:23 -0300

    add headerlocation, event created

--- src/main/java/com/manage/api/resource/CategoriaResource.java
+++ src/main/java/com/manage/api/resource/CategoriaResource.java
@@ -1,12 +1,13 @@
 package com.manage.api.resource;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.manage.api.event.RecursoCriadoEvent;
 import com.manage.api.model.Categoria;
 import com.manage.api.repository.CategoriaRepository;
 
@@ -26,18 +27,12 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
 	@GetMapping
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
-	}
-
-	@PostMapping
-	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
-		Categoria categoriaSalva = categoriaRepository.save(categoria);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
-		return ResponseEntity.created(uri).body(categoriaSalva);
 	}
 
 	@GetMapping("/{codigo}")
@@ -45,4 +40,13 @@ public class CategoriaResource {
 		Categoria categoria = categoriaRepository.findOne(codigo);
 		return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
 	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
+	}
+
 }
